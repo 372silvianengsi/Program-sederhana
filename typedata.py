cb408ade7412b19c343db8f7ebd1cab8198e8fea
372silvianengsi <93824294+372silvianengsi@users.noreply.github.com>
2021-11-18 16:46:12 +0700

    Add files via upload

---
+++ typedata.py
@@ -0,0 +1,13 @@
+print("PROGRAM PHYTON MEMASAK TELUR")
+memasak telur = float(input("\orak-arik telur : "))
+memasak telur = float(input("telur ceplok : "))
+memasak telur = float(input("telur rebus : "))
+
+na = (memasak telur)
+if na >= telur ceplok: 
+elif na >= 'nyalakan api kompor'
+elif na >= 'tuangkan minyak kedalam wajan'
+elif na >= 'pecahkan telur kedalam wajan'
+elif na >= 'beri garam dan penyedap rasa secukupnya'
+else: 'masak telur hingga matang'
+print("\memasak telur na")
